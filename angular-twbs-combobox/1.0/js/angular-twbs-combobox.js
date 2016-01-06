935279b70a19a5e9ab0ef8113be96f051e1cf2c5
generalsaku <josefgdev@gmail.com>
2016-01-06 15:38:21 +0100

    trim text which has spacing to left and right.

--- angular-twbs-combobox/1.0/js/angular-twbs-combobox.js
+++ angular-twbs-combobox/1.0/js/angular-twbs-combobox.js
@@ -130,7 +130,7 @@
             if (!val)
                 val = this.$menu.find('li').attr('data-value');
 
-            this.$element.val(this.updater($(val).text())).trigger('change');
+            this.$element.val(this.updater($(val).text().replace(/^\s\s*/, '').replace(/\s\s*$/, ''))).trigger('change');
             this.$target.val(this.map[val]).trigger('change');
             this.$source.val(this.map[val]).trigger('change');
             this.$container.addClass('combobox-selected');
